86de0208b352da68f1a25e0397100e47d7608e8f
Maik PCgamer <mdfontechad@unal.edu.co>
2025-01-16 14:52:22 -0500

    models

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,10 +74,18 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#Settings.py 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # Specifies the database engine (MySQL)
+        'NAME': 'LittleLemon',  # Name of the database
+        'USER': 'root',  # MySQL username
+        'PASSWORD': 'mdfd0406',  # MySQL password (currently empty)
+        'HOST': '127.0.0.1',  # Database host (localhost)
+        'PORT': '3306',  # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  # MySQL options
+        }
     }
 }
 
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# BookingTable Model
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)  # Automatically generates a unique ID
+    name = models.CharField(max_length=255)  # Name field with max length 255
+    no_of_guests = models.IntegerField()  # Number of guests, with an integer constraint
+    booking_date = models.DateTimeField()  # Date and time of booking
+
+    def __str__(self):
+        return f"{self.name} - {self.booking_date}"
+
+
+# MenuTable Model
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)  # Automatically generates a unique ID
+    title = models.CharField(max_length=255)  # Title of the menu item
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Price with up to 10 digits, 2 decimal places
+    inventory = models.IntegerField()  # Inventory count with integer constraint
+
+    def __str__(self):
+        return f"{self.title} - ${self.price}"
